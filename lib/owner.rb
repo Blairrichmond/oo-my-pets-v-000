55b859dbf5252e5167e571e55ab9e3f32a8a7326
Brent  Ransom <sadumedusa@gmail.com>
2018-02-18 06:48:22 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -54,6 +54,10 @@ class Owner
     self.pets[:fishes].each {|fish| fish.mood = "happy"}
   end
 
+  def sell_pets
+    @pets.each {|pet| pet.mood = "nervous"}
+  end
+
   def list_pets
     "I have #{self.pets[:fishes].count} fish, #{self.pets[:dogs].count} dog(s), and #{self.pets[:cats].count} cat(s)."
   end
